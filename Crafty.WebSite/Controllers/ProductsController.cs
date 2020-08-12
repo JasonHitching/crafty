bc991ec24960761c5c81b754c0889944a9f54b72
Jason Hitching <jasonhitching1994@gmail.com>
2020-08-12 15:42:42 +0100

    modified: products controller class
    Added HttpGet functionality to the class to enable the retrieval of product data

--- Crafty.WebSite/Controllers/ProductsController.cs
+++ Crafty.WebSite/Controllers/ProductsController.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Crafty.WebSite.Models;
+using Crafty.WebSite.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Crafty.WebSite.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        public JsonFileProductService productService { get; }
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return productService.GetProducts();
+        }
     }
+
 }

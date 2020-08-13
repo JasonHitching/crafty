748f9e853105aef6859861ed0db9882b5ac5e0e3
Jason Hitching <jasonhitching1994@gmail.com>
2020-08-13 16:07:06 +0100

    removed: uneccesary imports

--- Crafty.WebSite/Startup.cs
+++ Crafty.WebSite/Startup.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Crafty.WebSite.Models;
 using Crafty.WebSite.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
